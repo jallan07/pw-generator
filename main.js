90f86b91f154e8df7b5aae9872ebd867fc533324
Josh Allan <allan.josh07@gmail.com>
2020-08-31 09:46:20 -0400

    Added a copy password button

--- main.js
+++ main.js
@@ -4,8 +4,10 @@
             var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         // An array for storing potential letters
             var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        // An arrat for storing special characters
+        // An array for storing special characters
             var special = '!@#$%^&*(){}[]|?.<>,/-=_+~`';
+        // An array for storing Star Wars phrases
+            var starWars = ["D@rth", "V@d3r", "S!d!ous", "L3!a", "Kyl*", "R3y", "!uk3", "H@n", "(hew!3", "Y0d@", "K2$O", "Ar2", "Dee2", "R2D2", "P@!pat!n3", "()rg@na", "S*!*", "Ahs*k@", "T@n*", "J3d!", "$!th", "Windu", "Ezr@", "Br!dg3R", "M!ll3nium", "FaLc0n"]
 
 // Functions for selecting random characters from the above arrays
         // Generates a random number
@@ -33,8 +35,15 @@
                 special: getRandomSpecial
             };
 
+// Star Wars function
+            function starWarsFunc() {
+                if (sw-toggle === true) {}
+            }
+
+
 // Debugging logs
     console.log(getRandomNumber());
     console.log(getRandomLower());
     console.log(getRandomUpper());
     console.log(getRandomSpecial());
+
