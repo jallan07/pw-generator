0f27beba92fc4d8dfc1297aca95eee00997238b0
Josh Allan <allan.josh07@gmail.com>
2020-09-01 10:55:09 -0400

    Enabled the PW generator function to push the value of the password into the textarea, along with other minor tweaks to the styling and organization of code.

--- main.js
+++ main.js
@@ -1,104 +1,84 @@
+// Various arrays for potential characters
+    var lowers = 'abcdefghijklmnopqrstuvwxyz';
+    var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numbers = '0123456789';
+    var special = '!@#$%^&*(){}[]|?.<>,/-=_+~`';
 
-// Variables for storing potential characters
-    // A variable for storing potential lowercase letters
-        var lowers = 'abcdefghijklmnopqrstuvwxyz';
-    // A variable for storing potential uppercase letters
-        var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    // A variable for storing potential numbers
-        var numbers = '0123456789';
-    // A variable for storing special characters
-        var special = '!@#$%^&*(){}[]|?.<>,/-=_+~`';
+// Set default character to empty
+    var charSet = "";
 
-// Give our charSet an initial character set of lowers so that it has at least some characters to work with
-        var charSet = lowers;
-
-// Set our password variable to an empty string that we will add to later
-        var password = "";
+// Set default password to empty — my function will fill it in later
+    var password = "";
 
 // Event listener for the "Create Password" button
-        runCriteria.addEventListener("click", function(){
+runCriteria.addEventListener("click", function(){
 
     // Check to make sure the pwLength variable is set properly
-            while(true) {
-                var pwLength = prompt("How many characters do you want your password to be?");
-                // Set the pwLength variable to a number instead of a string
-                var pwLength = +pwLength;
-                // Run if statements to make sure the length fits in the project scope criteria
-                if (pwLength < 8){
-                    alert("Passwords must be at least 8 characters in length.");
-                } 
-                else if (pwLength > 128) {
-                    alert("Passwords must be no more than 128 characters in length.");
-                }
-                else {
-                    break;
-                }
-            };
+    while(true) {
+        var pwLength = prompt("How many characters do you want your password to be?");
+        // Set the pwLength variable to a number instead of a string
+        var pwLength = +pwLength;
+        // Run if statements to make sure the length fits in the project scope criteria
+        if (pwLength < 8){
+            alert("Passwords must be at least 8 characters in length.");
+        } 
+        else if (pwLength > 128) {
+            alert("Passwords must be no more than 128 characters in length.");
+        }
+        else {
+            break;
+        }
+    };
 
-        // Create the variables for the other pieces of criteria
-            // var lowersPrompt = confirm("Click OK if you would like to include lowercase letters, and CANCEL if not.");
-            var uppersPrompt = confirm("Click OK if you would like to include uppercase letters, and CANCEL if not.");
-            var numbersPrompt = confirm("Click OK if you would like to include numbers, and CANCEL if not.");
-            var specialPrompt = confirm("Click OK if you would like to include special characters, and CANCEL if not.");
+    // Create the variables for the other pieces of criteria
+    var lowersPrompt = confirm("Click OK if you would like to include lowercase letters, and CANCEL if not.");
+    var uppersPrompt = confirm("Click OK if you would like to include uppercase letters, and CANCEL if not.");
+    var numbersPrompt = confirm("Click OK if you would like to include numbers, and CANCEL if not.");
+    var specialPrompt = confirm("Click OK if you would like to include special characters, and CANCEL if not.");
 
-        // Adjust charSet based on the criteria requested by the user
-            // if they would like to include 
-            if (uppersPrompt === true && numbersPrompt === true && specialPrompt === true) {
-                charSet += uppers;
-                charSet += numbers;
-                charSet += special;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt === true && numbersPrompt === true && specialPrompt !== true) {
-                charSet += uppers;
-                charSet += numbers;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt === true && numbersPrompt !== true && specialPrompt === true) {
-                charSet += uppers;
-                charSet += special;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt === true && numbersPrompt !== true && specialPrompt !== true) {
-                charSet += uppers;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt !== true && numbersPrompt === true && specialPrompt === true) {
-                charSet += numbers;
-                charSet += special;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt !== true && numbersPrompt === true && specialPrompt !== true) {
-                charSet += numbers;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else if (uppersPrompt !== true && numbersPrompt !== true && specialPrompt === true) {
-                charSet += special;
-                console.log(masterPasswordGenerator(pwLength, charSet));
-                return charSet;
-            }
-            else {
-                alert("You must select at least 1 character type to create a password. Please start again.")
-            }
-        }); 
+    // Adjust charSet based on the criteria requested by the user
+    while (true) {
+        if (lowersPrompt === true){
+            charSet += lowers;
+        }
+        if (uppersPrompt === true){
+            charSet += uppers;
+        }
+        if (numbersPrompt === true){
+            charSet += numbers;
+        }
+        if (specialPrompt === true){
+            charSet += special;
+        }
+        break;
+    };
+    console.log(masterPasswordGenerator(pwLength, charSet));
 
-// Master generator function
-            function masterPasswordGenerator(pwLength, charSet) {
-                for (i = 0; i < pwLength; i++) {
-                    password += charSet.charAt(Math.floor(Math.random() * charSet.length));
-                }
-                return password;
-            };
+    // Master generator function
+    function masterPasswordGenerator(pwLength, charSet) {
+        for (i = 0; i < pwLength; i++) {
+            password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+        }
+        return password;
+    };
+    console.log(password);
+    document.getElementById("pwResult").innerHTML += password;
+    // document.getElementById("mainHeading").innerHTML = "Pa$$woRd G3n3rat0r";
 
-            // console.log(password);
+}); 
 
 
+
+
+
+// —————————————————————————————————————————— //
+// —————————————————————————————————————————— //
+// —————————————————————————————————————————— //
+// —————————————————————————————————————————— //
+
+
+
+// OLD Foundational Functions
 
 // // Functions for selecting random characters from the above arrays
 //         // Generates a random number
@@ -121,3 +101,7 @@
 //             function getRandomSW() {
 //                 return starWars[Math.floor(Math.random() * starWars.length)];
 //             }
+
+
+// FOR USE LATER:
+// var starWars = ["Luk3", "Skyw@Lk3r", "H@n", "S0l0", "L3ia", "J3d!", "$!th", "R3y", "Kyl*", "D*7th", "V*d3r", "Y*d@", "$!d!ous"];
